fcf533bf150b23b63b7096672350591007f8609a
mziz <z.milda.z@gmail.com>
2013-01-18 12:19:04 -0500

    added example yaml/demo class skeleton

--- lab0/src/MessagePasser.java
+++ lab0/src/MessagePasser.java
@@ -3,8 +3,11 @@ import java.util.*;
 import java.net.*;
 
 public class MessagePasser {
- public MessagePasser(String configuration_filename, String local_name) {
-	 
+ public MessagePasser(String configuration_filename, String local_name) throws IOException {
+	 FileInputStream fstream = new FileInputStream(configuration_filename);
+	 DataInputStream in = new DataInputStream(fstream);
+	 BufferedReader cfBuffer = new BufferedReader(new InputStreamReader(in));
+	 System.out.println(cfBuffer.readLine());
  }
  
  void send(Message message) {
