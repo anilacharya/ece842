be87eaa070e7d3a249f4dec16d5a53dacf1bfd68
milda <milda@SEAWOLF-ubu.(none)>
2013-01-18 12:18:15 -0500

    added example yaml/demo class skeleton

---
+++ lab0/src/Demo.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+
+public class Demo {
+	public static void main(String[] args) throws IOException {
+		MessagePasser mp = new MessagePasser("Lab0.yaml", "my_box");
+	}
+}
