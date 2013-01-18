9de33ce4ea1a3daa8cb19ff23a8b2c579b8c0dc2
milda <milda@SEAWOLF-ubu.(none)>
2013-01-18 11:19:31 -0500

    eclipse project with skeleton code committed

---
+++ lab0/src/Message.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import java.net.*;
+
+public class Message implements Serializable {
+	
+ public Message(String src, String dest, String kind, Object data) {
+	 
+ }
+ public void set_id(int id) { // used by MessagePasser.send, not your app
+	 
+ }
+ 
+ // other accessors, toString, etc as needed
+}
---
+++ lab0/src/MessagePasser.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.net.*;
+
+public class MessagePasser {
+ public MessagePasser(String configuration_filename, String local_name) {
+	 
+ }
+ 
+ void send(Message message) {
+	 
+ }
+ 
+ Message receive( ) {// may block
+	 return null;
+ }
+}
