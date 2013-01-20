7837ce47ed977dd4c01f9cba8939ea791818c725
anilacharya <aaachary@andrew.cmu.edu>
2013-01-20 15:01:19 -0500

    Update lab0/src/Message.java

--- lab0/src/Message.java
+++ lab0/src/Message.java
@@ -1,15 +1,30 @@
 import java.io.*;
-import java.util.*;
-import java.net.*;
+//import java.util.*;
+//import java.net.*;
 
 public class Message implements Serializable {
+
+	/**
+	 * default serial version id 
+	 */
+	private static final long serialVersionUID = 1L;
 	
- public Message(String src, String dest, String kind, Object data) {
-	 
- }
- public void set_id(int id) { // used by MessagePasser.send, not your app
-	 
- }
+	public int messageId;
+	public String messageKind;
+	public String messageSource;
+	public String messageDest;
+	public Object messageData;
+	
+	public Message(String src, String dest, String kind, Object data) {
+		messageSource = src;
+		messageDest = dest;
+		messageKind = kind;
+		messageData = data;
+	}
+	
+	public void setId(int id) { // used by MessagePasser.send, not your app
+		this.messageId = id;
+	}
  
  // other accessors, toString, etc as needed
 }
